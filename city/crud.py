9d599832c2365713e66a5b7dc6a09255671999dd
ladno338 <pda130507@gmail.com>
2024-06-19 11:14:13 +0300

    Solution

--- city/crud.py
+++ city/crud.py
@@ -1,4 +1,5 @@
 from sqlalchemy import select
+from sqlalchemy.orm import query
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -6,19 +7,19 @@ from city import models
 from city import schemas
 
 
-async def get_all_cities(db: AsyncSession):
+async def get_all_cities(db: AsyncSession) -> list:
     query = select(models.DBCity)
     cities_list = await db.execute(query)
     return [city[0] for city in cities_list.fetchall()]
 
 
-async def get_city_by_name(db: AsyncSession, name: str):
+async def get_city_by_name(db: AsyncSession, name: str) -> query:
     query = select(models.DBCity).filter(models.DBCity.name == name)
     result = await db.execute(query)
     return result.scalars().first()
 
 
-async def create_city(db: AsyncSession, city: schemas.CityCreate):
+async def create_city(db: AsyncSession, city: schemas.CityCreate) -> query:
     db_city = models.DBCity(
         name=city.name,
         additional_info=city.additional_info,
@@ -29,6 +30,6 @@ async def create_city(db: AsyncSession, city: schemas.CityCreate):
     return db_city
 
 
-async def delete_city(db: AsyncSession, city_id: int):
+async def delete_city(db: AsyncSession, city_id: int) -> query:
     result = await db.execute(models.DBCity.delete().where(models.DBCity.id == city_id))
     return result.rowcount
--- temperature/crud.py
+++ temperature/crud.py
@@ -1,23 +1,25 @@
 from sqlalchemy import select
+from sqlalchemy.orm import query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from . import models
 from . import schemas
 
 
-async def get_all_temperatures(db: AsyncSession):
+async def get_all_temperatures(db: AsyncSession) -> query:
     query = select(models.DBTemperature)
     temperatures_list = await db.execute(query)
     return [temperature[0] for temperature in temperatures_list.fetchall()]
 
 
-async def get_temperature(db: AsyncSession, city_id: int):
+async def get_temperature(db: AsyncSession, city_id: int) -> query:
     query = select(models.DBTemperature).filter(models.DBTemperature.city_id == city_id)
     result = await db.execute(query)
     return result.scalars().first()
 
 
-async def create_temperatures(db: AsyncSession, temperatures: list[schemas.TemperatureCreate]):
+async def create_temperatures(db: AsyncSession,
+                              temperatures: list[schemas.TemperatureCreate]) -> query:
     async for temperature in temperatures:
         db_temperature = models.DBTemperature(
             city_id=temperature.city_id,
